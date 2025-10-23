d1de01ffca90e7b3d270291e7d4d8f79ac06df6c
beiker <beikercamacho007@fgmail.com>
2025-10-22 21:21:30 -0400

    se termino la vista del mayor

--- src/Cl_vAgenciaViajes.ts
+++ src/Cl_vAgenciaViajes.ts
@@ -16,7 +16,7 @@ export default class Cl_vAgenciaViajes extends Cl_vGeneral{
     protected _vInternacional: Cl_vInternacional;
     private dataPaqueteTuristico: HTMLElement;
     private lblTotalVendido: HTMLElement;
-    private lblPaquetesInternacionales: HTMLElement;
+    private lblCantPaquetesInternacionales: HTMLElement;
     private lblPorcPaqInternacionales: HTMLElement;
     private btAgregarNacional: HTMLElement;
     private btAgregarInternacional: HTMLElement;
@@ -25,6 +25,67 @@ export default class Cl_vAgenciaViajes extends Cl_vGeneral{
         super({formName:"mainForm"});
         this._vNacional = new Cl_vNacional();
         this._vInternacional= new Cl_vInternacional();
-        this.
+        this.dataPaqueteTuristico = this.createHTMLElement({elementName: "dataPaqueteTuristico"});
+        this.lblTotalVendido = this.createHTMLElement({elementName:"lblTotalVendido"});
+        this.lblCantPaquetesInternacionales = this.createHTMLElement({elementName: "lblCantPaquetesInternacionales"});
+        this.lblPorcPaqInternacionales = this.createHTMLElement({elementName:"lblPorcPaqInternacionales"});
+        this.btAgregarNacional = this.creaHTMLButtonElement({
+            elementName: "btAgregarNacional",
+            onclick: ()=> {
+                this.show({ver:false});
+                this.vNacional.show();
+            },
+        });
+        this.btAgregarInternacional = this.creaHTMLButtonElement({
+            elementName:"btAgregarInternacional",
+            onclick: () => {
+                this.show({ver: false});
+                this.vInternacional.show();
+            },
+        });
+        this.dataPaqueteTuristico.innerHTML= "";
+        this.vNacional.show({ver:false});
+        this.vInternacional.show({ver:false});
+    }
+    set controlador(controlador:Cl_controlador){
+        super.controlador = controlador;
+        this.vNacional.controlador = controlador;
+        this.vInternacional.controlador = controlador;
+    }
+    get vNacional(){
+        return this._vNacional;
+    }
+    get vInternacional(){
+        return this._vInternacional;
+    }
+    reportarPaqueteTuristico({
+        dataPaqueteTuristico,
+        totalVendido,
+        porcPaquetesInternacionales,
+        cantPaquetesInternacionales,
+    }:{
+            dataPaqueteTuristico: iPaqueteTuristico;
+            totalVendido: number;
+            porcPaquetesInternacionales: number;
+            cantPaquetesInternacionales: number;
+    }): void{
+        this.dataPaqueteTuristico.innerHTML += `
+        <td class="colNumber">${`$ ${dataPaqueteTuristico.costo.toFixed(2)}`}</td>
+        <td class="colNumber">${dataPaqueteTuristico.codigo}</td>
+        <td class="colNumber">${dataPaqueteTuristico.destino ? dataPaqueteTuristico.destino:"--"}</td>
+        <td class="colCurrency">${`$${dataPaqueteTuristico.descuento?.toFixed(2) ? dataPaqueteTuristico.descuento: "--"}`}</td>
+        <td class="colCurrency">${`$ ${dataPaqueteTuristico.pagar.toFixed(2)}`}</td>
+    `;
+    this.lblTotalVendido.innerHTML = totalVendido.toFixed(2);
+    this.lblPorcPaqInternacionales.innerHTML = porcPaquetesInternacionales.toFixed(2);
+    this.lblCantPaquetesInternacionales.innerHTML = cantPaquetesInternacionales.toFixed();
+    }
+
+    show({ ver = true }: { ver?: boolean; } = {ver: true}): void {
+        super.show({ver});
+        if(ver){
+            this.vNacional.show({ver: false});
+            this.vInternacional.show({ver: false});
+        }
     }
 }
